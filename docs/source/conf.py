f031a7e57b0d83842fa4ed23134875ea801af6b3
D-ShorelineS <118434369+D-ShorelineS@users.noreply.github.com>
2022-11-24 17:19:25 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ShorelineS'
+copyright = '2020, IHE Delft & Deltares'
+author = 'Mostafa Saleh'
 
 release = '0.1'
 version = '0.1.0'
